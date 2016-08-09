27eac2463c2b60b6f1636c2af78161cf0a34e244
Justin Jent <jentjr@gmail.com>
2016-08-08 23:52:53 -0400

    added setup.py for build and moved source files and tests

---
+++ setup.py
@@ -0,0 +1,20 @@
+from timml import __version__
+from __future__ import division, absolute_import, print_function
+from numpy.distutils.core import Extension
+
+ext = Extension(name = 'besselaes',
+                sources = ['src/besselaes.f90'])
+
+if __name__ == "__main__":
+    from numpy.distutils.core import setup
+    setup(name = 'timml',
+          version=__version__,
+          packages = ['timml'],
+          install_requires = ['numpy>=1.9', 'scipy', 'matplotlib>=1.4'],
+          description = "TimML, A Multi-Layer, Analytical Element Model",
+          author = 'Mark Bakker',
+          author_email = 'mark.bakker@tudelft.nl',
+          url = 'https://github.com/mbakker7/timml',
+          license = 'MIT',
+          ext_modules = [ext]
+          )
