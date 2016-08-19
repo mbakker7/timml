155245f49c87eac8b9700122a26c7888d7dee324
Justin Jent <jentjr@gmail.com>
2016-08-18 20:30:00 -0400

    fix imports for installation

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from __future__ import division, absolute_import, print_function
-from timml import __version__
-from numpy.distutils.core import Extension
+import sys
+import os
+
+try:
+    from numpy.distutils.core import Extension, setup
+except ImportError:
+    sys.exit("install requires: 'numpy'.")
+
+f_compile_args = []
 
 ext = Extension(name = 'besselaes',
-                sources = ['src/besselaes.f90'])
+                sources = ['src/besselaes.f90'],
+                extra_f90_compile_args=f_compile_args,
+                )
 
 if __name__ == "__main__":
     from numpy.distutils.core import setup
     setup(name = 'timml',
-          version=__version__,
+          version="4.0.4",
           packages = ['timml'],
           install_requires = ['numpy>=1.9', 'scipy', 'matplotlib>=1.4'],
           description = "TimML, A Multi-Layer, Analytical Element Model",
