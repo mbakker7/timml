cb3c7b98586c368aab5e13c449864d3f58250568
Mark Bakker <markbak@gmail.com>
2017-07-26 15:34:12 +0200

    added color to contour

--- timml/util.py
+++ timml/util.py
@@ -7,20 +7,20 @@ class PlotTim:
         for e in self.elementlist:
             e.plot()
     
-    def contour(self, x1, x2, nx, y1, y2, ny, layers, levels, layout=True, labels=False, decimals=0, newfig=True):
+    def contour(self, x1, x2, nx, y1, y2, ny, layers, levels, layout=True, labels=False, decimals=0, color=None, newfig=True):
         layers = np.atleast_1d(layers)
         xg = np.linspace(x1, x2, nx)
         yg = np.linspace(y1, y2, ny)
         h = self.headgrid2(x1, x2, nx, y1, y2, ny, layers)
         if newfig: plt.figure()
         for ilayer in range(len(layers)):
-            cs = plt.contour(xg, yg, h[ilayer], levels)
+            cs = plt.contour(xg, yg, h[ilayer], levels, colors=color)
             if labels:
                 fmt = '%1.' + str(decimals) + 'f'
                 plt.clabel(cs, fmt=fmt)
         plt.axis('scaled')
         if layout: self.plot()
-        plt.show()
+        #plt.show()
         
     def tracelines(self, xstart, ystart, zstart, hstepmax, vstepfrac=0.2, tmax=1e12, nstepmax=100, silent='.', color=None, horizontal=True, win=[-1e30, 1e30, -1e30, 1e30]):
         xyztlist = timtracelines(self, xstart, ystart, zstart, hstepmax=hstepmax, vstepfrac=vstepfrac, tmax=tmax, nstepmax=nstepmax, silent=silent, win=win)
