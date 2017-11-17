50891817a724147ed69beb3c5698654b199a00ea
Justin Jent <jentjr@gmail.com>
2017-11-16 23:08:58 -0500

    working on travis build

--- setup.py
+++ setup.py
@@ -1,59 +1,41 @@
-from setuptools import setup
+import sys
+import os
 
-#from setuptools import find_packages  
-#from os import path
-#from codecs import open  # To use a consistent encoding
-#here = path.abspath(path.dirname(__file__))
-#
-# Get the long description from the relevant file
-#with open(path.join(here, 'README'), encoding='utf-8') as f:
-#    long_description = f.read()
-
-from timml import __version__
-
-l_d = ''
 try:
-   import pypandoc
-   l_d = pypandoc.convert('README.md', 'rst')
-except:
-   pass  
+    from numpy.distutils.core import Extension, setup
+except ImportError:
+    sys.exit("install requires: 'numpy'.")
 
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
 
-setup(
-    name='timml',
+    config = Configuration('timml', parent_package, top_path)
+    config.add_extension('besselaesnew',
+                         sources = ['timml/src/besselaesnew.f95',
+                             'timml/src/besselaesnew.pyf'])
 
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # http://packaging.python.org/en/latest/tutorial.html#version
-    version = __version__,
+    config.get_version(os.path.join('timml', 'version.py'))
 
-    description = 'Steady multi-layer AEM model',
-    long_description = l_d,
+    config.make_config_py()
+    return config
 
-    # The project's main homepage.
-    url = 'https://github.com/mbakker7/timml',
+def setup_package():
 
-    # Author details
-    author='Mark Bakker',
-    author_email='markbak@gmail.com',
+    metadata = dict(
+        name = 'timml',
+        version="5.0.0",
+        install_requires = ['numpy>=1.12', 'scipy>=0.19', 'matplotlib>=2.0'],
+        description = "TimML, A Multi-Layer, Analytical Element Model",
+        author = 'Mark Bakker',
+        author_email = 'mark.bakker@tudelft.nl',
+        url = 'https://github.com/mbakker7/timml',
+        license = 'MIT',
+        packages = ['timml'],
+        )
 
-    # Choose your license
-    license = 'MIT',
+    metadata['configuration'] = configuration
 
-    # See https://PyPI.python.org/PyPI?%3Aaction=list_classifiers
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        # Indicate who your project is intended for
-        #'Intended Audience :: Groundwater Modelers',
-        # Pick yor license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.6'
-        ],
-    platforms='Windows, Mac OS-X',
-    install_requires=['numpy>=1.12', 'matplotlib>=2.0'],
-    packages=['timml'],
-    include_package_data = True,
-    package_data = {'timml': ['src/besselaesnew.f95', 'besselaesnew.cpython-36m-darwin.so', 'besselaesnew.cp36-win_amd64.pyd']}
-    )
+    setup(**metadata)
+
+if __name__ == '__main__':
+    setup_package()
