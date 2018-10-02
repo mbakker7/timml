8ff579a84f60c7244eb2c06c10daca02a88770be
jentjr <jentjr@gmail.com>
2018-10-02 11:28:43 -0400

    added test for import and fix yield deprecation warning

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 import shutil, tempfile
 
 nbdir = os.path.join('notebooks')
@@ -8,10 +9,30 @@ testdir = tempfile.mkdtemp()
 def get_notebooks():
     return [f for f in os.listdir(nbdir) if f.endswith('.ipynb')]
 
-def run_notebook(fn):
+def get_jupyter_kernel():
+    try:
+        jklcmd = ('jupyter', 'kernelspec', 'list')
+        b = subprocess.Popen(jklcmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
+        if isinstance(b, bytes):
+            b = b.decode('utf-8')
+        print(b)
+        for line in b.splitlines():
+            if 'python' in line:
+                kernel = line.split()[0]
+    except:
+        kernel = None
+    
+    return kernel    
+
+@pytest.mark.parametrize("fn", get_notebooks())
+def test_notebook(fn):
+    
+    kernel = get_jupyter_kernel()
+    print('available jupyter kernel {}'.format(kernel))
+
     pth = os.path.join(nbdir, fn)
+    
     cmd = 'jupyter ' + 'nbconvert ' + \
-          '--ExecutePreprocessor.kernel_name=python ' + \
           '--ExecutePreprocessor.timeout=600 ' + '--to ' + 'notebook ' + \
           '--execute ' + '{} '.format(pth) + \
           '--output-dir ' + '{} '.format(testdir) + \
@@ -19,12 +40,7 @@ def run_notebook(fn):
     ival = os.system(cmd)
     assert ival == 0, 'could not run {}'.format(fn)
 
-def test_notebooks():
-    files = get_notebooks()
-
-    for fn in files:
-        yield run_notebook, fn
 
 if __name__ == '__main__':
-    test_notebooks()
+    test_notebook()
     shutil.rmtree(testdir)
