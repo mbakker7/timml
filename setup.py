08a111cc13484ef126490bc2b9e79205cb61c70f
Raoul Collenteur <raoulcollenteur@gmail.com>
2019-05-20 14:15:05 +0200

    Add classifier: Topic :: Scientific/Engineering :: Hydrology
    Suggest to use this classifier in an effort to centrally start collecting Python packages for use in hydrogical studies. (https://github.com/pypa/warehouse/issues/5728)

--- setup.py
+++ setup.py
@@ -24,4 +24,7 @@ setup(
     packages=["timml", "timml/besselaesnumba"],
     python_requires='>3.5',
     install_requires=["numpy>=1.12", "scipy>=0.19", "numba>=0.40", "matplotlib>=2.0"],
+    classifiers=[
+        'Topic :: Scientific/Engineering :: Hydrology',
+    ],
 )
