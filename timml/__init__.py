6f682eb5a3c995bad66b369c54d0625460894edb
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2023-07-05 12:50:22 +0200

    Fix #51 (#57)
    * add bessel.py for importing/switching besselaes module
    
    * fix #51
    - remove f2py from elements and model class
    - move besselaes module logic to bessel.py
    - refactor bessel function calls
    - set bessel method in init
    
    * update building pit nb
    
    * add besselaes benchmarking notebook
    - show difference fortran numba
    - with and without multiprocessing
    
    * do not test benchmark nb
    
    * do not test benchmark nb
    
    * update building pit nb

--- timml/__init__.py
+++ timml/__init__.py
@@ -1,4 +1,4 @@
-'''
+"""
 Copyright (C), 2015, Mark Bakker.
 Mark Bakker, Delft University of Technology
 mark dot bakker at tudelft dot nl
@@ -6,23 +6,33 @@ mark dot bakker at tudelft dot nl
 TimML is a computer program for the simulation of steady-state
 multiaquifer flow with analytic elements and consists of a
 library of Python scripts and FORTRAN extensions.
-'''
-#from __future__ import division, print_function, absolute_import
+"""
+# from __future__ import division, print_function, absolute_import
 
-#--version number
-__name__='timml'
-__author__='Mark Bakker'
+# --version number
+__name__ = "timml"
+__author__ = "Mark Bakker"
 from .version import __version__
 
 # Import all classes and functions
 from .model import ModelMaq, Model3D, Model
 from .well import WellBase, Well, HeadWell
 from .constant import Constant, ConstantStar
-from .linesink import LineSinkBase, HeadLineSinkZero, HeadLineSink, \
-                      LineSinkDitch, HeadLineSinkString, LineSinkDitchString, \
-                      HeadLineSinkContainer
-from .linedoublet import ImpLineDoublet, ImpLineDoubletString, \
-                         LeakyLineDoublet, LeakyLineDoubletString
+from .linesink import (
+    LineSinkBase,
+    HeadLineSinkZero,
+    HeadLineSink,
+    LineSinkDitch,
+    HeadLineSinkString,
+    LineSinkDitchString,
+    HeadLineSinkContainer,
+)
+from .linedoublet import (
+    ImpLineDoublet,
+    ImpLineDoubletString,
+    LeakyLineDoublet,
+    LeakyLineDoubletString,
+)
 from .circareasink import CircAreaSink
 from .inhomogeneity import PolygonInhomMaq, PolygonInhom3D, BuildingPit
 from .inhomogeneity1d import StripInhomMaq, StripInhom3D
@@ -31,5 +41,9 @@ from .trace import timtraceline, timtracelines
 from .linesink1d import LineSink1D, HeadLineSink1D
 from .linedoublet1d import ImpLineDoublet1D, LeakyLineDoublet1D
 from .stripareasink import StripAreaSink
+from . import bessel
 
 __all__ = [s for s in dir() if not s.startswith("_")]
+
+# default bessel module is numba
+bessel.set_bessel_method(method="numba")
