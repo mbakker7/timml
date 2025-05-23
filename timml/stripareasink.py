78702cc4e0bef92de017b97dc375254e4b2345d4
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2025-05-23 14:35:14 +0200

    remove circareasink changetrace method from XsectionAreaSinkInhom

--- timml/stripareasink.py
+++ timml/stripareasink.py
@@ -86,41 +86,6 @@ class XsectionAreaSinkInhom(Element):
             ]  # minus cause the parameter is the infiltration rate
         return rv
 
-    def changetrace(
-        self, xyzt1, xyzt2, aq, layer, ltype, modellayer, direction, hstepmax
-    ):
-        changed = False
-        terminate = False
-        xyztnew = 0
-        message = None
-        eps = 1e-8
-        r1sq = (xyzt1[0] - self.xc) ** 2 + (xyzt1[1] - self.yc) ** 2
-        r2sq = (xyzt2[0] - self.xc) ** 2 + (xyzt2[1] - self.yc) ** 2
-        if (r1sq < self.Rsq and r2sq > self.Rsq) or (
-            r1sq > self.Rsq and r2sq < self.Rsq
-        ):
-            changed = True
-            x1, y1 = xyzt1[0:2]
-            x2, y2 = xyzt2[0:2]
-            a = (x2 - x1) ** 2 + (y2 - y1) ** 2
-            b = 2 * ((x2 - x1) * (x1 - self.xc) + (y2 - y1) * (y1 - self.yc))
-            c = (
-                self.xc**2
-                + self.yc**2
-                + x1**2
-                + y1**2
-                - 2 * (self.xc * x1 + self.yc * y1)
-                - self.Rsq
-            )
-            u1 = (-b - np.sqrt(b**2 - 4 * a * c)) / (2 * a)
-            u2 = (-b + np.sqrt(b**2 - 4 * a * c)) / (2 * a)
-            if u1 > 0:
-                u = u1 * (1.0 + eps)  # Go just beyond circle
-            else:
-                u = u2 * (1.0 + eps)  # Go just beyond circle
-            xyztnew = xyzt1 + u * (xyzt2 - xyzt1)
-        return changed, terminate, xyztnew, message
-
 
 class XsectionAreaSink(Element):
     def __init__(
